aed796500478cb53d7dd7d63daf24f96596412b7
Lance Feldman <lance.a.b.feldman@gmail.com>
2025-04-28 19:49:04 +0930

    update_walli

--- quiz.js
+++ quiz.js
@@ -1,37 +1,102 @@
-let questions=["what is diddys real name?"]
-let answer=[
+let questions = ["what is diddys real name?", "Am I good at bedwars", "Am I a boy?"]
+let answers = [
     [
         "sean", "YOUR MUM"
+    ],
+    ["good", "bad"
+
+    ],
+    [
+        "Yes", "No"
     ]
 ]
-let RightAnswer=[
-    'sean'
+let RightAnswer = [
+    'sean',
+    'good',
+    'Yes'
 ]
 
-let question=document.getElementById('question')
-let answer1= document.getElementById('answer1')
-let answer2= document.getElementById('wronganswer1')
+let huzz = document.getElementById('question')
+let answer1 = document.getElementById('answer1')
+let answer2 = document.getElementById('wronganswer1')
 
-question.innerText=questions[0]
-answer1.innerText=answer[0][0]
-answer2.innerText=answer[0][1]
+let score = 0
+let QuestionNumber = 0
+let numOfQuest = questions.length
+let answer = ''
+
+function showQuestion(question) {
+    huzz.innerText = questions[question]
+    answer1.innerText = answers[question][0]
+    answer2.innerText = answers[question][1]
+    answer = RightAnswer[question]
+}
+
+showQuestion(0)
+
+function checkScore() {
+    if (score == numOfQuest) {
+        right()
+    } else {
+        wrong()
+    }
+}
+
+function right() {
+    document.location = "yougotitright.html"
+}
+
+function wrong() {
+    document.location = "yougotitwrong.html"
+}
+
+function isLastQuestionOrSomething() {
+    if (
+        QuestionNumber >= numOfQuest
+    ) {
+        return true
+    } else {
+        return false
+    }
+}
+
 
 answer1.addEventListener("click", function (e) {
-    if (answer1.innerText===RightAnswer[0]) {
-        document.location = "yougotitright.html"
-        alert('You got it correct!')
-    } else{
-        alert(' YOU GOT IT WRONG! EMOTIONAL DAMAGE!')
-        document.open('yougotitwrong.html')
-        document.location = "yougotitwrong.html"
+    if (answer1.innerText === answer) {
+        score = score + 1
     }
-  });
-  answer2.addEventListener("click", function (e) {
-    if (answer2.innerText===RightAnswer[0]) {
-        alert('You got it correct!')
-        document.location = "yougotitright.html"
-    } else{
-        alert(' YOU GOT IT WRONG! EMOTIONAL DAMAGE!')
-        document.location = "yougotitwrong.html"
+    QuestionNumber = QuestionNumber + 1
+    if (isLastQuestionOrSomething() == true) {
+        checkScore()
+    } else {
+        showQuestion(QuestionNumber)
     }
-  });
+});
+answer2.addEventListener("click", function (e) {
+    if (answer2.innerText === answer) {
+        score = score + 1
+    }
+    QuestionNumber = QuestionNumber + 1
+    if (isLastQuestionOrSomething() == true) {
+        checkScore()
+    } else {
+        showQuestion(QuestionNumber)
+    }
+});
+
+
+window.addEventListener('pageshow', function (event) {
+    if (event.persisted) {
+        // This page was restored from the cache, reset variables here
+        resetVariables();
+    }
+});
+
+function resetVariables() {
+    score = 0
+    QuestionNumber = 0
+    numOfQuest = questions.length
+    answer = ''
+    showQuestion(0)
+
+}
