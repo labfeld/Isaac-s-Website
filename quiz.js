4d228d551b977035750a33f3eeef64ee2f71675b
Lance Feldman <lance.a.b.feldman@gmail.com>
2025-05-05 19:52:04 +0930

    Update 1.16

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
-let questions = ["what is diddys real name?", "Am I good at bedwars", "Am I a boy?", "Why do I play Bedwars?"]
+let questions = ["what is diddys real name?", "Am I good at bedwars", "Am I a boy?", "Why do I play Bedwars?",
+    "Where was I born", "Am I bald"
+]
 let answers = [
     [
         "sean", "YOUR MUM"
@@ -10,21 +12,32 @@ let answers = [
         "Yes", "No"
     ],
     [
-        "Cuz I feel like it", "Because Wallibear plays bedwars.", 'Cuz Im bald', 'My IQ is 50'
+        "Cuz I feel like it", "Because Wallibear plays bedwars.", 'Cuz Im bald', 'My IQ is 69'
+    ],
+    [
+        "America", "China", "IDK", "Australia"
+    ],
+    [
+        "Ya", "No"
     ]
-    
+
 ]
 let RightAnswer = [
     'sean',
     'good',
     'Yes',
-    'Cuz I feel like it',
-    'Because Wallibear plays bedwars.'
-]   
+    'Because Wallibear plays bedwars.',
+    'America',
+    'Ya'
+]
 
 let huzz = document.getElementById('question')
 let answer1 = document.getElementById('answer1')
-let answer2 = document.getElementById('wronganswer1')
+let answer2 = document.getElementById('answer2')
+let answer3 = document.getElementById('answer3')
+let answer4 = document.getElementById('answer4')
+let secondRowButtons = document.getElementById('secondRowButtons')
+
 
 let score = 0
 let QuestionNumber = 0
@@ -33,8 +46,20 @@ let answer = ''
 
 function showQuestion(question) {
     huzz.innerText = questions[question]
-    answer1.innerText = answers[question][0]
-    answer2.innerText = answers[question][1]
+    let QuestionAnswers = answers[question]
+    answer1.innerText = QuestionAnswers[0]
+    answer2.innerText = QuestionAnswers[1]
+
+    if (QuestionAnswers.length == 2) {
+        secondRowButtons.classList.add("hide")
+    } else if (
+        QuestionAnswers.length == 4
+    ) {
+        secondRowButtons.classList.remove("hide")
+        answer3.innerText = QuestionAnswers[2]
+        answer4.innerText = QuestionAnswers[3]
+    }
+
     answer = RightAnswer[question]
 }
 
@@ -66,37 +91,26 @@ function isLastQuestionOrSomething() {
     }
 }
 
+function ButtonSetup(button) {
+    button.addEventListener("click", function (e) {
+        if (button.innerText === answer) {
+            score = score + 1
+        }
+        QuestionNumber = QuestionNumber + 1
+        if (isLastQuestionOrSomething() == true) {
+            checkScore()
+        } else {
 
-answer1.addEventListener("click", function (e) {
-    if (answer1.innerText === answer) {
-        score = score + 1
-    }
-    QuestionNumber = QuestionNumber + 1
-    if (isLastQuestionOrSomething() == true) {
-        checkScore()
-    } else {
-        showQuestion(QuestionNumber)
-    }
-});
-answer2.addEventListener("click", function (e) {
-    if (answer2.innerText === answer) {
-        score = score + 1
-    }
-    QuestionNumber = QuestionNumber + 1
-    if (isLastQuestionOrSomething() == true) {
-        checkScore()
-    } else {
-        showQuestion(QuestionNumber)
-    }
-});
-
-
-window.addEventListener('pageshow', function (event) {
-    if (event.persisted) {
-        // This page was restored from the cache, reset variables here
-        resetVariables();
-    }
-});
+setTimeout(function() {
+    showQuestion(QuestionNumber)  }, 1);
+            
+        }
+    });
+}
+ ButtonSetup(answer1)
+ ButtonSetup(answer2)
+ ButtonSetup(answer3)
+ ButtonSetup(answer4)
 
 function resetVariables() {
     score = 0
